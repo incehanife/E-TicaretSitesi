e09786cbee100880df39ff2bbf0385560649d2e7
incehanife <incehanifee@gmail.com>
2020-08-18 22:16:42 +0300

    ilk commit

--- home/views.py
+++ home/views.py
@@ -1,8 +1,82 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from home.models import ContactFormu, ContactFormMessage, Setting
+from product.models import Product, Category
+
+
 def index(request):
-    text="Merhaba Django"
-    context = {'text': text}
-    return render(request, 'index.html', context)
+    setting = Setting.objects.get(pk=4)
+    sliderdata = Product.objects.all()[:4]
+    category=Category.objects.all()
+
+    context={'setting': setting,
+             'category': category,
+             'page':'home',
+             'sliderdata':sliderdata}
+    return render(request, 'index.html',context)
+
+def hakkimizda(request):
+    category = Category.objects.all()
+    setting = Setting.objects.get(pk=4)
+    context = {'setting': setting,
+               'category': category}
+    return render(request, 'hakkimizda.html', context)
+
+def iletisim(request):
+    category = Category.objects.all()
+    setting = Setting.objects.get(pk=4)
+    context = {'setting': setting,
+               'category': category}
+    return render(request, 'iletisim.html', context)
+
+def referanslar(request):
+    category = Category.objects.all()
+    setting = Setting.objects.get(pk=4)
+    context = {'setting': setting,
+               'category': category}
+    return render(request, 'referanslar.html', context)
+
+def iletisim(request):
+
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Your message has been sent successfully. Thank you!")
+            return HttpResponseRedirect('/iletisim')
+    category = Category.objects.all()
+    setting = Setting.objects.get(pk=4)
+    form = ContactFormu()
+    context={'setting':setting,'form':form,'category': category}
+    return render(request,'iletisim.html',context)
+
+def category_products(request,id,slug):
+    category = Category.objects.all()
+    setting = Setting.objects.get(pk=4)
+    products = Product.objects.filter(category_id=id)
+    context = {'products': products,'category': category,'setting': setting}
+    return render(request, 'products.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- product/models.py
+++ product/models.py
@@ -1,52 +1,95 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from ckeditor_uploader.fields import RichTextUploadingField
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
+
+
+class Category(MPTTModel):
     STATUS = (
-        ('True','Evet'),
+        ('True', 'Evet'),
         ('False', 'Hayır'),
     )
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=225)
     keywords = models.CharField(max_length=225)
-    image = models.ImageField(blank=True,upload_to='images/')
-    status = models.CharField(max_length=10, choices=STATUS,)
-    slug = models.SlugField()
-    parent = models.ForeignKey('self',blank=True, null=True, related_name='children',on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS, )
+    slug = models.SlugField(null=False, unique=True)
+    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
+    class MPTTMeta:
+        order_insertion_by = ['title']
 
-class Product(object):
-    pass
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        full_path = [self.title]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.title)
+            k = k.parent
+        return ' >> '.join(full_path[::-1])
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
 
 
 class Product(models.Model):
     STATUS = (
-        ('True','Evet'),
+        ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    category =models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     description = models.CharField(max_length=225)
     keywords = models.CharField(max_length=225)
-    image = models.ImageField(blank=True,upload_to='images/')
-    price =models.FloatField()
-    amount =models.IntegerField()
-    detail = models.TextField()
-    status = models.CharField(max_length=10, choices=STATUS,)
+    image = models.ImageField(blank=True, upload_to='images/', default='images/default.jpg', null=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2,default=0)
+    amount = models.IntegerField()
+    detail = RichTextUploadingField()
+    slug = models.SlugField(blank=True, max_length=150,null=False, unique=True)
+    status = models.CharField(max_length=10, choices=STATUS, )
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
+    def image_tag(self):
+        if self.image:
+            return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+            image_tag.short_description = 'Image'
+        return '#'
+
+    def catimg_tag(self):
+        return mark_safe((Category.status))
+
+
 class Images(models.Model):
-        product=models.ForeignKey(Product,on_delete=models.CASCADE)
-        title = models.CharField(max_length=50,blank=True)
-        image = models.ImageField(blank=True, upload_to='images/')
-        def __str__(self):
-            return self.title
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+
+
+
+

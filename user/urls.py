6cf4920a2d8907126232b80623393b68c6d7ecce
incehanife <incehanifee@gmail.com>
2020-08-22 20:58:33 +0300

    first commit

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # ex: /home/
     path('', views.index, name='index'),
+    path('orders', views.orders, name='orders'),
 
     # ex: /home/5/
     #path('<int:question_id>/', views.detail, name='detail'),
--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
-
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from home.models import Setting, UserProfile
+from order.models import Order
 from product.models import Category
 
 
@@ -16,3 +18,14 @@ def index(request):
                'setting': setting,
                'category': category,}
     return render(request,'user_profile.html',context)
+
+@login_required(login_url='/login')
+def orders(request):
+    setting = Setting.objects.get(pk=4)
+    category = Category.objects.all()
+    current_user = request.user
+    orders= Order.objects.filter(user_id=current_user.id)
+    context = {'orders': orders,
+               'setting': setting,
+               'category': category,}
+    return render(request,'user_orders.html',context)
